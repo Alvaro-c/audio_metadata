c7f5001bfa233160beb363a22e46ebee2a77f783
alvaroc <alvarocanasboal@gmail.com>
2024-10-30 07:58:53 +0100

    Add one tag

--- upgrade.py
+++ upgrade.py
@@ -1,8 +1,14 @@
+import argparse
+import json
+import subprocess
+from ffmpy import FFprobe
+import mutagen
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, ID3NoHeaderError, TIT2, TPE1, TALB, TCON, TDRC, TPOS, TRCK, TXXX
 
-class track:
-    class tag_id3v2:
+
+class Track:
+    class TagID3v2:
         title = 'TIT2'
         artist = 'TPE1'
         tracknumber = 'TRCK'
@@ -26,17 +32,14 @@ class track:
         releasestatus = 'TXXX:MusicBrainz Album Status'
         releasecountry = 'TXXX:MusicBrainz Album Release Country'
         acoustid_id = 'TXXX:Acoustid Id'
-        musicbrainz_albumid = 'TXXX:MusicBrainz Album Id' 
+        musicbrainz_albumid = 'TXXX:MusicBrainz Album Id'
         musicbrainz_artistid = 'TXXX:MusicBrainz Artist Id'
         musicbrainz_albumartistid = 'TXXX:MusicBrainz Album Artist Id'
         musicbrainz_releasegroupid = 'TXXX:MusicBrainz Release Group Id'
         musicbrainz_trackid = 'TXXX:MusicBrainz Release Track Id'
         acoustid_fingerprint = 'TXXX:Acoustid Fingerprint'
-        
 
-
-
-    class tag_vorbis:
+    class TagVorbis:
         title = 'TITLE'
         artist = 'ARTIST'
         tracknumber = 'TRACKNUMBER'
@@ -66,30 +69,64 @@ class track:
         musicbrainz_releasegroupid = 'MUSICBRAINZ_RELEASEGROUPID'
         musicbrainz_trackid = 'MUSICBRAINZ_RELEASETRACKID'
         acoustid_fingerprint = 'ACOUSTID_FINGERPRINT'
-    
-    
-    
-    
-    title = ''
+
+    def __init__(self, format):
+        if format == 'mp3':
+            tag_class = Track.TagID3v2
+
+        elif format == 'flac':
+            tag_class = Track.TagVorbis
+        else:
+            raise ValueError('Unsupported format')
+
+        for attr in dir(tag_class):
+            if not attr.startswith('__'):
+                setattr(self, attr, getattr(tag_class, attr))
+
+
+parser = argparse.ArgumentParser(
+    "Extract metadata from mp3 files to JSON files")
+parser.add_argument(
+    "low_audio", help="Path to the audio that will be removed", type=str)
+parser.add_argument(
+    "high_audio", help="Path to the audio that will be added", type=str)
+
+
+def get_metadata(audio_file):
+    probe = FFprobe(
+        global_options='-v quiet -print_format json -show_format',
+        inputs={audio_file: None}
+    )
+
+    stdout, stderr = probe.run(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    metadata = json.loads(stdout.decode('utf-8'))
+    return metadata['format']['tags'], metadata['format']['format_name']
+
 
 def main():
-    # 1. Get both files
-    original = ''
-    upgrade = ''
+    args = parser.parse_args()
+
+    # 1. Get metadata from low audio
+    metadata = get_metadata(args.low_audio)[0]
+    _, format_high = get_metadata(args.high_audio)
+
+    if format_high == 'mp3':
+        high_audio = ID3(args.high_audio)
+        high_audio.add(TIT2(encoding=3, text=metadata['title']))
+        high_audio.save()
+
 
     # 2. Copy tags from origin
 
-    
-    # 3. Remove original
 
-    # 4. Copy upgrade to destination
+    # 4. Copy upgrade to destination with the same name, so path is the same
 
     # 5. Remove tags in new
-
 
     # 5. Add tags to destination
 
     # 6. Calculate Audio Fingerprint
+
 
 
 if __name__ == "__main__":
